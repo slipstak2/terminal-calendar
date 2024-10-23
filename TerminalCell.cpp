4681b477cc5efd51d21a2f8858bfe271587d6708
slipstak2@yandex.ru <slipstak2@yandex.ru>
2024-10-23 08:50:24 +0300

    font true color + text style (underline): but slowly

--- TerminalCell.cpp
+++ TerminalCell.cpp
@@ -54,12 +54,24 @@ bool TerminalCell::operator != (const TerminalCell& rhs) {
 // https://habr.com/ru/companies/macloud/articles/558316/
 void TerminalCell::Render() const {
     auto fmtSettings = GetFormatSettings();
+    
     printf("\033[%d;%d;%dm%s\033[0m", 
         (int)(fmtSettings.textStyle), 
         int(fmtSettings.backgroundColor), 
         (int)fmtSettings.fontColor, 
         rune.get()
     );
+
+    // \033[53
+    // \033[38;2;201;200;59m    полный спектр rbg для цвета и фона
+    // \033[38;5;120            256 цветов для шрифта и фона
+
+
+    // true color + text style (underline)
+    //printf("\033[%dm", (int)(fmtSettings.textStyle));
+    //printf("\033[38;2;201;200;59m"); // fontColor;
+    //printf("%s\033[0m", rune.get());
+
 
     // \033[38;2;<r>;<g>;<b>m           #Select RGB foreground color
     // \033[48; 2; <r>; <g>; <b>m       #Select RGB background color
