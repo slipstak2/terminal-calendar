74af1ca2f3a5d69329638d33a805f6c53932a602
slipstak2 <slipstak2@yandex.ru>
2024-11-29 17:11:28 +0300

    add pause in run-tests.bat

--- run-tests.bat
+++ run-tests.bat
@@ -6,6 +6,8 @@ CALL :RunTests .\x64\Release\common-tests.exe
 CALL :RunTests .\x64\Debug\terminal-controls-tests.exe
 CALL :RunTests .\x64\Release\terminal-controls-tests.exe
 
+pause
+
 :RunTests
 %~1
 if %ERRORLEVEL% NEQ 0 (
