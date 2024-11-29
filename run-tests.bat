43539815f6dee1335aaebd2716534130d28dd1a6
slipstak2 <slipstak2@yandex.ru>
2024-11-29 17:10:15 +0300

    add batch file for running all tests in Debug and Release configurations

---
+++ run-tests.bat
@@ -0,0 +1,13 @@
+@echo off
+
+CALL :RunTests .\x64\Debug\common-tests.exe
+CALL :RunTests .\x64\Release\common-tests.exe
+
+CALL :RunTests .\x64\Debug\terminal-controls-tests.exe
+CALL :RunTests .\x64\Release\terminal-controls-tests.exe
+
+:RunTests
+%~1
+if %ERRORLEVEL% NEQ 0 (
+	exit -1
+)
