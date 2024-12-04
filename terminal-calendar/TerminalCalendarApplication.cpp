baa103b1439e16da8d6f22edec0df579353c4bc0
slipstak2 <slipstak2@yandex.ru>
2024-12-04 20:23:33 +0300

    add props files for all projects

---
+++ terminal-calendar/TerminalCalendarApplication.cpp
@@ -0,0 +1,21 @@
+#include "common.h"
+#include "TerminalApplication.h"
+#include "TerminalCanvas.h"
+#include "TerminalLabel.h"
+#include "TerminalButton.h"
+#include "TerminalWindow.h"
+#include "TerminalRootControl.h"
+#include "TerminalLabelSwitcher.h"
+#include "DataProviders/ListDataProvider.h"
+#include "TerminalListView.h"
+#include "TerminalRadioButton.h"
+#include "TerminalBorderListView.h"
+#include "TerminalCheckBox.h"
+#include "TerminalControlsConfig.h"
+
+#include "TerminalCalendarApplication.h"
+
+TerminalCalendarApplication::TerminalCalendarApplication()
+    : TerminalApplication() {
+
+}
---
+++ terminal-calendar/TerminalCalendarApplication.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "TerminalApplication.h"
+
+class TerminalCalendarApplication : public TerminalApplication {
+public:
+    TerminalCalendarApplication();
+};
