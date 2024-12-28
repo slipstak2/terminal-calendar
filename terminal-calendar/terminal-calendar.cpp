e709698f2ce9e98dd76ae760545840389ed1446d
slipstak2 <slipstak2@yandex.ru>
2024-12-28 18:54:19 +0300

    add field map example

--- terminal-calendar/terminal-calendar.cpp
+++ terminal-calendar/terminal-calendar.cpp
@@ -1,4 +1,6 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
 #include <Windows.h>
 #include "TerminalCalendarApplication.h"
 
@@ -6,8 +8,14 @@ int main()
 {
     SetConsoleOutputCP(CP_UTF8);
 
-    TerminalCalendarApplication app;
-    app.Run();
+    //TerminalCalendarApplication app;
+    //app.Run();
+
+    freopen("field.txt", "r", stdin);
+    std::string row;
+    while (getline(std::cin, row)) {
+        std::cout << row << std::endl;
+    }
 
     return 0;
 }
