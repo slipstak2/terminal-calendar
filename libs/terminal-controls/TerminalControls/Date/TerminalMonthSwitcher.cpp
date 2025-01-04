74012f2ecad88d2b3fbc5a037536b8d292a15db0
slipstak2@yandex.ru <slipstak2@yandex.ru>
2025-01-04 22:54:44 +0300

    add month label

--- libs/terminal-controls/TerminalControls/Date/TerminalMonthSwitcher.cpp
+++ libs/terminal-controls/TerminalControls/Date/TerminalMonthSwitcher.cpp
@@ -1,8 +1,11 @@
 #include "TerminalMonthSwitcher.h"
+#include "TerminalLabel.h"
 
 TerminalMonthSwitcher::TerminalMonthSwitcher(int month, TerminalCoord position)
     : month(month)
     , TerminalBorderControl("", position, TerminalSize{.height = 8, .width = 24})
 {
+    auto monthLabel = TerminalLabel::Create("Март", TerminalCoord{ .row = 0, .col = 9 });
+    AddControlOnBorder(monthLabel);
 
 }
